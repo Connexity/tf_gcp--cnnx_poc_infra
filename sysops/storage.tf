5733f544e257396ab47b7ec2541507d51610c0d5
Justin McDonald LDAP Account <jmcdonald@sysopsjobstage001.bo.shopzilla.seastg>
2022-05-10 14:23:07 -0700

    connexity.com > skimlinks.co.uk

--- sysops/storage.tf
+++ sysops/storage.tf
@@ -10,14 +10,6 @@ resource "google_storage_bucket" "sysops-test_cnnx-poc-infra" {
   }
 }
 
-data "google_iam_policy" "sysops-objectadmin_sysops-test_cnnx-poc-infra" {
-  binding {
-    role = "roles/storage.admin"
-    members = [
-      "user:pharrison@connexity.com",
-    ]
-  }
-}
 
 resource "google_storage_bucket_iam_policy" "policy" {
   bucket = google_storage_bucket.sysops-test_cnnx-poc-infra.name
