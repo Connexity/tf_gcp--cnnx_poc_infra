86dd2c5ed7cea297629081f8f4c469ca5274a4c6
Roman Gazaryants <rgazaryants@shopzilla.com>
2022-06-01 20:42:49 -0700

    +hostname

--- infrastructure/rundeck-stage.tf
+++ infrastructure/rundeck-stage.tf
@@ -1,5 +1,6 @@
 resource "google_compute_instance" "rundeck-stage" {
   name = "rundeckstage001"
+  hostname = "rundeckstage001.stage-usc1g.connexity.net"
   machine_type = "e2-medium"
   zone         = "us-central1-a"
   tags = ["allow-ingress", "allow-gce-usc1-stage", "allow-onprem"]
