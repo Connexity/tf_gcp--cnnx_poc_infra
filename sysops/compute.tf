d7a9cfa9eb48088e22944e931529d0e53c250cb9
Justin McDonald LDAP Account <jmcdonald@sysopsjobstage001.bo.shopzilla.seastg>
2022-08-11 14:19:26 -0700

    test for_each

--- sysops/compute.tf
+++ sysops/compute.tf
@@ -1,11 +1,4 @@
 locals {
-  it_names = toset([
-    "test-it-stage001", 
-    "test-it-stage003", 
-   ])
-}
-
-locals {
   it_attached_disk_names = toset([
     "test-it-stage001-1",
     "test-it-stage003-1",
@@ -24,9 +17,8 @@ resource "google_compute_disk" "test-it-stage" {
     }
 }
 
-resource "google_compute_instance" "test-it-stage" {
-    for_each = local.it_names
-    name = each.value
+resource "google_compute_instance_template" "test-it-stage" {
+    name = "test-it-stage"
     machine_type = "e2-highmem-4"
     zone = "us-central1-a"
 
@@ -48,13 +40,11 @@ resource "google_compute_instance" "test-it-stage" {
 
     tags = ["allow-gce-lb", "allow-gce-usc1-stage", "allow-onprem"]
 
-    boot_disk {
-      device_name = each.value
-      initialize_params {
-      image = "projects/cnnx-infra-osimages/global/images/family/cnnx-ubuntu-2004-lts" 
+    disk {
+      source_image      = "projects/cnnx-infra-osimages/global/images/family/cnnx-ubuntu-2004-lts"
+      auto_delete       = true
+      boot              = true
       }
-    }
-    
    
     network_interface {
       subnetwork         = "https://www.googleapis.com/compute/v1/projects/cnnx-infra-networking/regions/us-central1/subnetworks/cnnx-usc1-stage-gce-1"
@@ -66,5 +56,13 @@ resource "google_compute_instance" "test-it-stage" {
     }
 
     metadata_startup_script = "http://gitlab.shopzilla.com/ansible/awx-boostrap-script/-/raw/master/awxprovision.py"
+
+}
+
+resource "google_compute_instance_from_template" "test-it-stage001" {
+  name = "test-it-stage001"
+  zone = "us-central1-a"
+
+  source_instance_template = google_compute_instance_template.test-it-stage.id
 
 }
