4c33df1fc71ff437fc3fa7006dbf5b1eabc2cace
Justin McDonald LDAP Account <jmcdonald@sysopsjobstage001.bo.shopzilla.seastg>
2022-08-11 08:53:03 -0700

    test multiple vm creation device names

--- sysops/compute.tf
+++ sysops/compute.tf
@@ -12,7 +12,7 @@ resource "google_compute_instance" "test-it-stage" {
     tags = ["allow-gce-lb", "allow-gce-usc1-stage", "allow-onprem"]
 
     boot_disk {
-    device_name = "${element(google_compute_instance.test-it-stage.name, count.index)}"
+    device_name = google_compute_instance.test-it-stage.name[count.index]
     initialize_params {
     image = "projects/cnnx-infra-osimages/global/images/family/cnnx-ubuntu-2004-lts"
     
