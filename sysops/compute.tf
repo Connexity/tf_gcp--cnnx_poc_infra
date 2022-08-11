e0f7587dbf8684bdcba1f1cf66b4ecff6359bf32
Justin McDonald LDAP Account <jmcdonald@sysopsjobstage001.bo.shopzilla.seastg>
2022-08-11 06:50:17 -0700

    test multiple vm creation

--- sysops/compute.tf
+++ sysops/compute.tf
@@ -1,19 +1,3 @@
-    count = "${var.it_node_count}"
-    name = "test-it-stage${count.index}-1"
-    machine_type = "e2-highmem-4"
-    zone = "us-central1-c"
-
-    tags = ["allow-gce-lb", "allow-gce-usc1-stage", "allow-onprem"]
-    
-    boot_disk {
-    initialize_params {
-    image = "projects/cnnx-infra-osimages/global/images/family/cnnx-ubuntu-2004-lts"
-    }
-   }
-    attached_disk {
-        source      = "${element(google_compute_disk.test-it-stage.*.self_link, count.index)}"
-        device_name = "${element(google_compute_disk.test-it-stage.*.name, count.index)}"
-   }
 variable "it_node_count" {
   default = "3"
  }
