4379fc7d7cbcdab07a6e120b68233ff13e6272ae
Justin McDonald LDAP Account <jmcdonald@sysopsjobstage001.bo.shopzilla.seastg>
2022-08-11 06:48:37 -0700

    test multiple vm creation

--- sysops/compute.tf
+++ sysops/compute.tf
@@ -1,46 +1,3 @@
-variable "it_node_count" {
-  default = "3"
- }
-
-resource "google_compute_instance_template" "cnnx-ubuntu-20-e2-highmem-4-50-150-ssd-usc1" {
-  name          = "cnnx-ubuntu-20-e2-highmem-50-150-ssd-usc1"
-  description   = "template for solr it 150 ssd attached"
-
-  labels  = {
-    owner = "platform"
-    app = "solr-it"
-  }
-
-  machine_type    = "e2-highmem-4"
-
-  disk {
-    source_image      = "projects/cnnx-infra-osimages/global/images/family/cnnx-ubuntu-2004-lts"
-    auto_delete       = true
-    boot              = true
-  }
-
-  metadata = {
-    env = "staging"
-  }
-
-  metadata_startup_script = "http://gitlab.shopzilla.com/ansible/awx-boostrap-script/-/raw/master/awxprovision.py"
-
-  network_interface {
-    subnetwork         = "https://www.googleapis.com/compute/v1/projects/cnnx-infra-networking/regions/us-central1/subnetworks/cnnx-usc1-infra-admin-gce-1"
-    subnetwork_project = "cnnx-infra-networking"
-  }
-   
-}
-
-resource "google_compute_disk" "test-it-stage" {
-    count   = "${var.it_node_count}"
-    name    = "test-it-stage${count.index}-1"
-    type    = "pd-standard"
-    zone    = "us-central1-c"
-    size    = "5"
-}
-
-resource "google_compute_instance" "test-it-stage" {
     count = "${var.it_node_count}"
     name = "test-it-stage${count.index}-1"
     machine_type = "e2-highmem-4"
@@ -57,11 +14,27 @@ resource "google_compute_instance" "test-it-stage" {
         source      = "${element(google_compute_disk.test-it-stage.*.self_link, count.index)}"
         device_name = "${element(google_compute_disk.test-it-stage.*.name, count.index)}"
    }
+variable "it_node_count" {
+  default = "3"
+ }
+
+resource "google_compute_instance" "test-it-stage" {
+    count = "${var.it_node_count}"
+    name = "${format{test-it-stage%03d-1, ${count.index})}"
+    machine_type = "e2-highmem-4"
+    zone = "us-central1-c"
+
+    tags = ["allow-gce-lb", "allow-gce-usc1-stage", "allow-onprem"]
+
+    boot_disk {
+    initialize_params {
+    image = "projects/cnnx-infra-osimages/global/images/family/cnnx-ubuntu-2004-lts"
+    }
+   }
 
     network_interface {
       subnetwork         = "https://www.googleapis.com/compute/v1/projects/cnnx-infra-networking/regions/us-central1/subnetworks/cnnx-usc1-stage-gce-1"
       subnetwork_project = "cnnx-infra-networking"
-    } 
+    }
 
 }
-
