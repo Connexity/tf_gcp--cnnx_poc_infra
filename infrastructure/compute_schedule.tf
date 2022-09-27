f36e34b728b737abe5b01a1254069462ee49f328
Philip Harrison <pharrison@pharrisonstage002.bo.shopzilla.seastg>
2022-09-27 06:30:47 -0700

    removing schedule-test01

--- infrastructure/compute_schedule.tf
+++ infrastructure/compute_schedule.tf
@@ -1,14 +1,14 @@
-resource "google_compute_resource_policy" "scheduke--eu_workingtime_test1" {
-  name   = "eu-workingtime-test1"
-  region = "us-central1"
-  description = "Start and stop instances"
-  instance_schedule_policy {
-    vm_start_schedule {
-      schedule = "0 9 * * 1-5"
-    }
-    vm_stop_schedule {
-      schedule = "30 10 * * 1-5"
-    }
-    time_zone = "Europe/Berlin"
-  }
-}
+#resource "google_compute_resource_policy" "scheduke--eu_workingtime_test1" {
+#  name   = "eu-workingtime-test1"
+#  region = "us-central1"
+#  description = "Start and stop instances"
+#  instance_schedule_policy {
+#    vm_start_schedule {
+#      schedule = "0 9 * * 1-5"
+#    }
+#    vm_stop_schedule {
+#      schedule = "30 10 * * 1-5"
+#    }
+#    time_zone = "Europe/Berlin"
+#  }
+#}
