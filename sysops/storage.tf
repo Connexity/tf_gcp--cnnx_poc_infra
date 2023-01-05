0f930605930b8e54864689197eddc098ba911fb9
Philip Harrison <pharrison@pharrisonstage002.bo.shopzilla.seastg>
2023-01-05 09:11:04 -0800

    testing gce multiwrite PD

--- sysops/storage.tf
+++ sysops/storage.tf
@@ -2,7 +2,7 @@ resource "google_storage_bucket" "sysops-test_cnnx-poc-infra" {
   name          = "sysops-test_cnnx-poc-infra"
   location      = "us-central1"
   force_destroy = true
-  storage_class = "standard"
+  storage_class = "STANDARD"
   uniform_bucket_level_access = true
   labels = { 
     "owner" = "sysops"
