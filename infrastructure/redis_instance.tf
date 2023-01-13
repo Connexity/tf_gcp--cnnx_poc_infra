239065f6760fab26ae57cc23a34e54ad1287e398
Kevin Donnelly LDAP Account <kdonnelly@kdonnellystage001.bo.shopzilla.seastg>
2023-01-13 10:34:36 -0800

    updating name

--- infrastructure/redis_instance.tf
+++ infrastructure/redis_instance.tf
@@ -1,5 +1,5 @@
 resource "google_redis_instance" "redis--test_service_cnnx-poc-infra" {
-  name           = "test_service_redis"
+  name           = "test-service-redis001"
   tier           = "BASIC"
   memory_size_gb = 1
 
