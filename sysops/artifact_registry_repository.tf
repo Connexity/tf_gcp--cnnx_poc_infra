c70a2b51fcde26726065dc8899f35cd805febdc5
Kevin Donnelly LDAP Account <kdonnelly@kdonnellystage001.bo.shopzilla.seastg>
2023-03-28 11:40:04 -0700

    Adding SA permissions

--- sysops/artifact_registry_repository.tf
+++ sysops/artifact_registry_repository.tf
@@ -9,3 +9,19 @@ resource "google_artifact_registry_repository" "cnnx-apt-focal-poc_cnnx-poc-infr
     "app" = "apt-repository"
   }
 }
+
+data "google_iam_policy" "cnnx-apt-focal-poc_iam_binding_policy" {
+  binding {
+    role = "roles/artifactregistry.reader"
+    members = [
+      "serviceAccount:logdump@cnnx-stage-tracking.iam.gserviceaccount.com",
+    ]
+  }
+}
+
+resource "google_artifact_registry_repository_iam_policy" "cnnx-apt-focal-poc_iam_policy" {
+  project = google_artifact_registry_repository.cnnx-apt-focal-poc_cnnx-poc-infra_artifact_registry.project
+  location = google_artifact_registry_repository.cnnx-apt-focal-poc_cnnx-poc-infra_artifact_registry.location
+  repository = google_artifact_registry_repository.cnnx-apt-focal-poc_cnnx-poc-infra_artifact_registry.name
+  policy_data = data.google_iam_policy.cnnx-apt-focal-poc_iam_binding_policy.policy_data
+}
