39aaf42cb6dfcc04e1d67ebb787298f3848b0324
Kevin Donnelly LDAP Account <kdonnelly@kdonnellystage001.bo.shopzilla.seastg>
2023-03-28 11:14:33 -0700

    Adding apt artifact repository

---
+++ sysops/artifact_registry_repository.tf
@@ -0,0 +1,10 @@
+resource "google_artifact_registry_repository" "cnnx-apt-focal-poc_cnnx-poc-infra_artifact_registry" {
+  location      = "us-central1"
+  repository_id = "cnnx-apt-focal-poc"
+  description   = "Repository for cnnx custom Ubuntu 20.04 (focal) packages. Test repo."
+  format        = "APT"
+  project       = "${var.gcp_project}"
+  labels = {
+    "owner" = "sysops"
+    "app" = "apt-repository"
+}
