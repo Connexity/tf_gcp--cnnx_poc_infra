b9948b01d83ae33c20f9bb77d7575c6e2fa01543
Kevin Donnelly LDAP Account <kdonnelly@kdonnellystage001.bo.shopzilla.seastg>
2023-04-27 14:27:41 -0700

    Adding dataproc job to test

---
+++ infrastructure/dataproc_job-test.tf
@@ -0,0 +1,16 @@
+resource "google_dataproc_job" "dataproc-hadoop-test" {
+  region       = "us-central1"
+  force_delete = true
+  placement {
+    cluster_name = "dataproc-test-cluster-gce"
+  }
+ 
+  hadoop_config {
+    main_jar_file_uri = "file:///usr/lib/hadoop-mapreduce/hadoop-mapreduce-examples.jar"
+    args = [
+      "wordcount",
+      "gs://dataproc-test-bucket_cnnx-poc-infra/Test.txt",
+      "gs://${google_dataproc_cluster.basic.cluster_config[0].bucket}/hadoopjob_output",
+    ]
+  }
+}
