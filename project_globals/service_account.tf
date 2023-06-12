fd885fe4fd3f01d0be66d3a44bef2775b82f4b17
elikhanov <elikhanov@connexity.com>
2023-06-12 15:03:56 +0200

    Added two SAs for the Migration Manager

--- project_globals/service_account.tf
+++ project_globals/service_account.tf
@@ -11,3 +11,17 @@ resource "google_service_account" "SA_dataproc" {
   disabled     = "false"
   display_name = "dataproc-test"
 }
+
+resource "google_service_account" "SA_migration-manager" {
+  account_id   = "migration-manager"
+  description  = "Migration Mmanager SA"
+  disabled     = "false"
+  display_name = "migration-manager"
+}
+
+resource "google_service_account" "SA_migration-cloud-extension" {
+  account_id   = "migration-cloud-extension"
+  description  = "Migration Cloud Extention SA"
+  disabled     = "false"
+  display_name = "migration-cloud-extension"
+}
