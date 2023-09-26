7b9952ea647b1e3cfbfb4d97b02754dae8a5f04c
Philip Harrison <pharrison@pharrisonstage101.bo.shopzilla.seastg>
2023-09-26 15:27:02 -0700

    testing bt

--- sysops/big_table.tf
+++ sysops/big_table.tf
@@ -19,36 +19,3 @@ resource "google_bigtable_instance" "bigtable-sysops-bt" {
   }
 }
 
-### inventory-instance End ###
-
-### inventory-instance_offer-id-mapping table Start ###
-
-resource "google_bigtable_table" "table_bigtable-sysops-bt_offer-id-mapping" {
-  name          = "offer-id-mapping"
-  instance_name = google_bigtable_instance.bigtable-sysops-bt.name
-  column_family {
-    family = "o"
-  }
-
-  lifecycle { 
-    prevent_destroy = true 
-  }
-}
-
-
-resource "google_bigtable_gc_policy" "table_table_bigtable-inventory-instance_offer-id-mapping_policy" {
-  instance_name = google_bigtable_instance.bigtable-sysops-bt.name
-  table         = google_bigtable_table.table_bigtable-sysops-bt_offer-id-mapping.name
-  column_family = "o"
-
-  mode = "UNION"
-
-  max_age {
-    duration = "1s"
-  }
-
-  max_version {
-    number = 1
-  }
-}
-
