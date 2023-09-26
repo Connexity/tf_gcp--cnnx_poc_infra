357b045a6a7a7126a9a46e0e13fe034baf8c9b92
Philip Harrison <pharrison@pharrisonstage101.bo.shopzilla.seastg>
2023-09-26 15:26:27 -0700

    testing bt

--- sysops/big_table.tf
+++ sysops/big_table.tf
@@ -4,7 +4,7 @@ resource "google_bigtable_instance" "bigtable-sysops-bt" {
   deletion_protection = false
   cluster {
     cluster_id   = "sysops-c1"
-    num_nodes    = 3
+    num_nodes    = 1
     storage_type = "SSD"
     zone = "us-central1-b"
   }
