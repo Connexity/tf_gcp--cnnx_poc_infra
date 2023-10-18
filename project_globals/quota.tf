5f456cd99c12958d373c5c35e2c6e9d48738f90e
Kevin Donnelly LDAP Account <kdonnelly@kdonnellystage001.bo.shopzilla.seastg>
2023-10-18 14:49:17 -0700

    changing quota to be just an integer.

--- project_globals/quota.tf
+++ project_globals/quota.tf
@@ -4,6 +4,6 @@ resource "google_service_usage_consumer_quota_override" "bq_usage_per_day" {
   service = "bigquery.googleapis.com"
   metric = urlencode("bigquery.googleapis.com/quota/query/usage")
   limit = urlencode("/d/project/user")
-  override_value = "5TiB"
+  override_value = "5"
   force = true
 }
