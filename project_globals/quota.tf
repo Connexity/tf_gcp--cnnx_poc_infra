f6aba5cd47acfb14a13e261533dacb5304848418
Kevin Donnelly LDAP Account <kdonnelly@kdonnellystage001.bo.shopzilla.seastg>
2023-10-18 14:39:53 -0700

    Testing quotas in stage

---
+++ project_globals/quota.tf
@@ -0,0 +1,9 @@
+resource "google_service_usage_consumer_quota_override" "bq_usage_per_day" {
+  provider = google-beta
+  project = "${var.gcp_project}"
+  service = "bigquery.googleapis.com"
+  metric = urlencode("bigquery.googleapis.com/quota/query/usage")
+  limit = urlencode("/d/project/user")
+  override_value = "5TiB"
+  force = true
+}
