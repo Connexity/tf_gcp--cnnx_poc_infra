1e5ddba968f3c3a6becb19c427f5de59ec2f4241
Kevin Donnelly LDAP Account <kdonnelly@kdonnellystage001.bo.shopzilla.seastg>
2024-03-11 17:00:35 -0700

    Deploying os_config patch deployment test

---
+++ sysops/os_config_patch_deployment-terraform-test_cnnx-poc-infra.tf
@@ -0,0 +1,42 @@
+resource "google_os_config_patch_deployment" "patch_deployment_terraform-test_cnnx-poc-infra" {
+  patch_deployment_id = "terraform-test_cnnx-poc-infra"
+
+  instance_filter {
+    instance_name_prefixes = "os-manage"
+    all = false
+    zones = ["us-central1-a","us-central1-b","us-central1-c","us-central1-f"]
+    group_labels {
+      labels = {
+        app = "osmanagementtest"
+      }
+    }
+  }
+
+  patch_config {
+    apt {
+      type = "UPGRADE"
+    }
+  }
+
+  rollout {
+    mode = "ZONE_BY_ZONE"
+    disruption_budget {
+      fixed = 1
+    }
+  }
+
+  recurring_schedule {
+    time_zone {
+      id = "America/Los_Angeles"
+    }
+    time_of_day {
+      hours = 10
+    }
+    monthly {
+      week_day_of_month {
+        week_ordinal = 1
+        day_of_week = "MONDAY"
+      }
+    }
+  }
+}
