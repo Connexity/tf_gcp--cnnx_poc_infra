41f96fe472dd51e8ef9390c36db0aa96bfadca19
elikhanov <elikhanov@connexity.com>
2024-07-10 13:41:36 +0200

    Adding Secret Manager Admin role to the terraform sa

--- project_globals/project_iam_member.tf
+++ project_globals/project_iam_member.tf
@@ -209,3 +209,9 @@ resource "google_project_iam_member" "SA_dataproc-service-account--cnnx-poc-infr
   role = "roles/dataproc.worker"
   project = "${var.gcp_project}"
 }
+resource "google_project_iam_member" "SA_terraform--cnnx-pos-infra--secretmanager_admin" {
+  member = "serviceAccount:terraform@cnnx-poc-infra.iam.gserviceaccount.com"
+  role = "roles/secretmanager.admin"
+  project = "${var.gcp_project}"
+}
+
