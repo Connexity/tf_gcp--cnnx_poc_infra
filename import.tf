0bcd451bd64b3ec0d4ff312dea2f29342473330e
Kevin Donnelly LDAP Account <kdonnelly@kdonnellystage001.bo.shopzilla.seastg>
2024-10-10 16:19:36 -0700

    testing again

--- import.tf
+++ import.tf
@@ -1,4 +1,4 @@
 import {
   id = "terraform_import_test"
-  to = module.infrastructure.google_storage_bucket.secret--terraform_import_test
+  to = module.infrastructure.google_secret_manager_secret.secret--terraform_import_test
 }
