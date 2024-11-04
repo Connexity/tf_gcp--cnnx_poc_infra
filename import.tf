f4bed95ff435f81d8a75f44073f7c8e52ed10968
Kevin Donnelly LDAP Account <kdonnelly@kdonnellystage001.bo.shopzilla.seastg>
2024-11-04 12:03:04 -0800

    Fixing typo

--- import.tf
+++ import.tf
@@ -1,4 +1,4 @@
 import {
-  id = "projects/cnnx-pof-infra/serviceAccounts/terraform-import-test@cnnx-poc-infra.iam.gserviceaccount.com"
+  id = "projects/cnnx-poc-infra/serviceAccounts/terraform-import-test@cnnx-poc-infra.iam.gserviceaccount.com"
   to = module.project_globals.google_service_account.SA_terraform-import-test
 }
