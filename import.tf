09eacf7251129bebe175b0d9b07bc498526aadc1
Kevin Donnelly LDAP Account <kdonnelly@kdonnellystage001.bo.shopzilla.seastg>
2024-11-04 13:29:46 -0800

    Removing test import

--- import.tf
+++
@@ -1,4 +0,0 @@
-import {
-  id = "projects/cnnx-poc-infra/serviceAccounts/terraform-import-test@cnnx-poc-infra.iam.gserviceaccount.com"
-  to = module.project_globals.google_service_account.SA_terraform-import-test
-}
