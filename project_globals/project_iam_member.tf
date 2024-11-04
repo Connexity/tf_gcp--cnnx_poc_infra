48ab9a20ba750fb5d622164a9bad61ddc2ed488b
Kevin Donnelly LDAP Account <kdonnelly@kdonnellystage001.bo.shopzilla.seastg>
2024-11-04 15:08:29 -0800

    Removing tests, tests successful

--- project_globals/project_iam_member.tf
+++ project_globals/project_iam_member.tf
@@ -215,9 +215,3 @@ resource "google_project_iam_member" "SA_terraform--cnnx-pos-infra--secretmanage
   role = "roles/secretmanager.admin"
   project = "${var.gcp_project}"
 }
-
-resource "google_project_iam_member" "SA_import-tests--cnnx-poc-infra--compute_viewer" {
-  member = "serviceAccount:import-tests@cnnx-poc-infra.iam.gserviceaccount.com"
-  role = "roles/compute.viewer"
-  project = "${var.gcp_project}"
-}
