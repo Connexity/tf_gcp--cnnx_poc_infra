c9f69eec3f5bc71cf2b5a5cd32cadd091e597ab2
Kevin Donnelly LDAP Account <kdonnelly@kdonnellystage001.bo.shopzilla.seastg>
2024-11-06 14:41:59 -0800

    Removing successful tests

--- import.tf
+++
@@ -1,9 +0,0 @@
-import {
-  id = "projects/cnnx-poc-infra/topics/test-terraform-import-topic"
-  to = module.infrastructure.google_pubsub_topic.pub-sub_test-terraform-import-topic_topic
-}
-
-import {
-  id = "projects/cnnx-poc-infra/subscriptions/test-terraform-import-topic-sub"
-  to = module.infrastructure.google_pubsub_subscription.pub-sub_test-terraform-import-topic-sub_topic_subscription
-}
--- project_globals/project_iam_member.tf
+++ project_globals/project_iam_member.tf
@@ -84,6 +84,12 @@ resource "google_project_iam_member" "SA_terraform--cnnx-poc-infra--artifactregi
   project = "${var.gcp_project}"
 }
 
+resource "google_project_iam_member" "SA_terraform--cnnx-poc-infra--pubsub_admin" {
+  member = "serviceAccount:terraform@cnnx-poc-infra.iam.gserviceaccount.com"
+  role = "roles/pubsub.admin"
+  project = "${var.gcp_project}"
+}
+
 resource "google_project_iam_member" "SA_gce_api--cnnx-poc-infra--compute_instanceAdmin_v1" {
   member = "serviceAccount:service-832312746254@compute-system.iam.gserviceaccount.com"
   role = "roles/compute.instanceAdmin.v1"
