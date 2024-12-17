7c71ab6a01b98183390cc4231537f2ea73de073e
Kevin Donnelly LDAP Account <kdonnelly@kdonnellystage001.bo.shopzilla.seastg>
2024-12-17 14:41:18 -0800

    Checking labels

--- sysops/disk-map-test.tf
+++ sysops/disk-map-test.tf
@@ -2,11 +2,11 @@ variable "map_persistent-disk" {
   description = "A way to create a persistent disk in different zones and sizes"
   type        = map(list(string))
   default = {
-    "mapped-disk01" = [ "pd-standard", "us-central1-a", "50" ],
-    "mapped-disk03" = [ "pd-ssd", "us-central1-f", "120" ],
-    "mapped-disk04" = [ "pd-standard", "us-central1-a", "50" ],
-    "mapped-disk05" = [ "pd-ssd", "us-central1-c", "100" ],
-    "mapped-disk06" = [ "pd-ssd", "us-central1-f", "50" ],
+    "mapped-disk01" = [ "pd-standard", "us-central1-a", "50", "app1" ],
+    "mapped-disk03" = [ "pd-ssd", "us-central1-f", "120", "app3" ],
+    "mapped-disk04" = [ "pd-standard", "us-central1-a", "50", "app4" ],
+    "mapped-disk05" = [ "pd-ssd", "us-central1-c", "100", "app5" ],
+    "mapped-disk06" = [ "pd-ssd", "us-central1-f", "50", "app6" ],
   }
 }
 
@@ -18,7 +18,7 @@ resource "google_compute_disk" "cnnx-poc-infra_persistent-disks" {
   zone     = each.value[1]
   size     = each.value[2]
   labels   = {
-    app = "sysops"
+    app = each.value[3]
     owner = "platform"
   }
 }
