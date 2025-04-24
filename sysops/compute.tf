d885a702db704add3cdbf386753413d713650391
Kevin Donnelly LDAP Account <kdonnelly@kdonnellystage001.bo.shopzilla.seastg>
2025-04-24 09:17:49 -0700

    Making test instance

--- sysops/compute.tf
+++ sysops/compute.tf
@@ -1,19 +1,19 @@
 locals {
-  test_solr_cluster_instance_count = 0
-  test_solr_cluster_name_prefix = "solr-test-stage"
+  test_solr_cluster_instance_count = 1
+  test_solr_cluster_name_prefix = "test-stage"
   test_solr_cluster_zone = "us-central1-c"
-  test_solr_cluster_machine_type = "n4-highmem-8"
+  test_solr_cluster_machine_type = "n4-standard-2"
   test_solr_cluster_labels = {
     owner = "sysops"
     app = "solr-test"
   }
   test_solr_cluster_email = "gce-sa@cnnx-poc-infra.iam.gserviceaccount.com"
   test_solr_cluster_tags = ["allow-onprem", "allow-gce-usc1-stage", "allow-gce-lb" ]
-  test_solr_cluster_umig_name = "lb-umig-usc1g-c-solr-test-stage-tf"
+  test_solr_cluster_umig_name = "lb-umig-usc1g-c-test-stage-tf"
 }
 
 
-resource "google_compute_instance" "Instance_solr-test-stage-instances" {
+resource "google_compute_instance" "Instance_test-stage-instances" {
   count = local.test_solr_cluster_instance_count
 
   project = "${var.gcp_project}"
@@ -79,7 +79,7 @@ resource "google_compute_instance_group" "lb_umig_usc1g_c_solr_test_stage_tf" {
   name = local.test_solr_cluster_umig_name
   zone = local.test_solr_cluster_zone
   project   = "${var.gcp_project}"
-  instances = [ for x in google_compute_instance.Instance_solr-test-stage-instances: x.self_link ]
+  instances = [ for x in google_compute_instance.Instance_test-stage-instances: x.self_link ]
   named_port {
     name = "tcp9100"
     port = "9100"
