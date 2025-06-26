2cb9dbe488c2b075c8b99cc64218260ccdccab23
Roman Gazaryants <rgazaryants@shopzilla.com>
2025-06-26 11:44:14 -0700

    trying to inherit project name from workspave name

--- main.tf
+++ main.tf
@@ -11,24 +11,23 @@ variable "TFC_WORKSPACE_NAME" {
 
 variable "gcp_project" {
   type = string
-  default = var.TFC_WORKSPACE_NAME
 }
 
 provider "google" {
-  project = "${var.gcp_project}"
+  project = "${var.TFC_WORKSPACE_NAME}"
 }
 
 module "project_globals" {
   source = "./project_globals"
-  gcp_project = "${var.gcp_project}"
+  gcp_project = "${var.TFC_WORKSPACE_NAME}"
 }
 
 module "infrastructure" {
   source = "./infrastructure"
-  gcp_project = "${var.gcp_project}"
+  gcp_project = "${var.TFC_WORKSPACE_NAME}"
 }
 
 module "sysops" {
   source = "./sysops"
-  gcp_project = "${var.gcp_project}"
+  gcp_project = "${var.TFC_WORKSPACE_NAME}"
 }
