78a2227ba7e3049fe2d219522af3b2e56e816298
Roman Gazaryants <rgazaryants@shopzilla.com>
2025-06-27 00:37:09 -0700

    trying to inherit project name from workspave name

--- main.tf
+++ main.tf
@@ -3,27 +3,27 @@ terraform {
   }
 }
 
-variable "TFC_WORKSPACE_NAME" {
+variable "gcp_project" {
   default     = ""
-  description = "Terraform Cloud workspace name"
+  description = "TF_VAR_gcp_project env variable"
   type        = string
 }
 
 provider "google" {
-  project = "${var.TFC_WORKSPACE_NAME}"
+  project = "${var.gcp_project}"
 }
 
 module "project_globals" {
   source = "./project_globals"
-  gcp_project = "${var.TFC_WORKSPACE_NAME}"
+  gcp_project = "${var.gcp_project}"
 }
 
 module "infrastructure" {
   source = "./infrastructure"
-  gcp_project = "${var.TFC_WORKSPACE_NAME}"
+  gcp_project = "${var.gcp_project}"
 }
 
 module "sysops" {
   source = "./sysops"
-  gcp_project = "${var.TFC_WORKSPACE_NAME}"
+  gcp_project = "${var.gcp_project}"
 }
