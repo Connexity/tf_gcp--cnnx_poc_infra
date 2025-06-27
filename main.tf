684f4b896c30cd193601f83916888c351b98e9c9
Roman Gazaryants <rgazaryants@shopzilla.com>
2025-06-27 14:13:37 -0700

    testing branch plan

--- main.tf
+++ main.tf
@@ -1,13 +1,18 @@
-terraform {
-  backend "http" {
-  }
-}
-
 variable "gcp_project" {
   default     = ""
   description = "$TF_VAR_gcp_project env variable. Copied from $TFC_WORKSPACE_NAME env variable"
   type        = string
 }
+
+terraform {
+  cloud {
+    organization = "Connexity"
+    workspaces {
+      name = "${var.gcp_project}"
+    }
+  }
+}
+
 
 provider "google" {
   project = "${var.gcp_project}"
